c4cf2da7fda1afa03b5d82b2aa27bb14a6e0263d
Fábio Uchôa <fabiohulopes@gmail.com>
2025-03-31 14:36:32 -0300

    Update classes_jogador.py

--- classes_jogador.py
+++ classes_jogador.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from sys import exit
 import math
 
+
 class Jogador:
     def __init__(self, window, x, y, largura, altura, cor, velocidade, cima, baixo, esquerda, direita):
         self.window = window
@@ -33,7 +34,7 @@ class Jogador:
 
     def tomar_dano(self):
         self.vida -= 1
-    
+
     def recuperar_vida(self):
         if self.vida < 3:
             self.vida += 1
@@ -46,24 +47,25 @@ class Jogador:
         pg.draw.rect(self.window, self.cor, (self.x, self.y, self.largura, self.altura))
         self.rect = pg.Rect(self.x, self.y, self.largura, self.altura)
 
-
     def interface(self):
-        fonte = pg.font.SysFont('pixel',35,bold=False,italic=True)
+        fonte = pg.font.SysFont('pixel', 35, bold=False, italic=True)
         mostrar_vida = fonte.render(f'Vida: {self.vida}', True, ('WHITE'))
         self.window.blit(mostrar_vida, (10, 10))
 
+
 class Tiro_Jogador:
     def __init__(self, x, y, mouse_x, mouse_y):
-        self.x = x
-        self.y = y
-        self.mouse_x = mouse_x
-        self.mouse_y = mouse_y
+        self.image = pg.image.load("img.png")  # Carrega a imagem da bola
+        self.image = pg.transform.scale(self.image, (20, 20))  # Redimensiona para 20x20 pixels
+        self.rect = self.image.get_rect(center=(x, y))  # Define o retângulo da bola
+
         self.speed = 6
-        self.angle = math.atan2(y - mouse_y, x - mouse_x)
+        self.angle = math.atan2(mouse_y - y, mouse_x - x)
         self.x_vel = math.cos(self.angle) * self.speed
         self.y_vel = math.sin(self.angle) * self.speed
 
     def update(self, display):
-        self.x -= int(self.x_vel)
-        self.y -= int(self.y_vel)
-        pg.draw.circle(display, (0, 0, 0), (self.x, self.y), 5)
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
+        display.blit(self.image, self.rect.topleft)  # Desenha a bola na tela
+
